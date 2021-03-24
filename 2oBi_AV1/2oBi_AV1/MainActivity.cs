8be60b6de7621f74174c2a01664b22b57bc5387d
José Augusto Mendes <zeca.saitou@gmail.com>
2021-03-24 14:32:33 -0300

    2º Bi - AV1
    2º Bi - AV1

---
+++ 2oBi_AV1/2oBi_AV1/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace _2oBi_AV1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Intent i;
+        Bundle parameters;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button btFB = FindViewById<Button>(Resource.Id.loginFB);
+            Button btGmail = FindViewById<Button>(Resource.Id.loginGmail);
+
+            btFB.Click += OnBtnFBClicked;
+            btGmail.Click += OnBtnGmailClicked;
+
+            i = new Intent(this, typeof(Profile));
+            parameters = new Bundle();
+        }
+
+        private void OnBtnGmailClicked(object sender, EventArgs e)
+        {
+            parameters.PutString("typeLogin", "gmail");
+            i.PutExtras(parameters);
+
+            StartActivity(i);
+        }
+
+        private void OnBtnFBClicked(object sender, EventArgs e)
+        {
+            parameters.PutString("typeLogin", "fb");
+            i.PutExtras(parameters);
+
+            StartActivity(i);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            parameters.Clear();
+        }
+    }
+}
---
+++ 2oBi_AV1/2oBi_AV1/Profile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace _2oBi_AV1
+{
+    [Activity(Label = "Profile")]
+    public class Profile : Activity
+    {
+        string typeLogin;
+        TextView tvTypeLogin;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.profile_layout);
+
+            Intent i = Intent;
+
+            if(i != null && i.Extras != null)
+            {
+                typeLogin = i.Extras.GetString("typeLogin");
+            }
+
+            tvTypeLogin = FindViewById<TextView>(Resource.Id.loginProfileWay);
+            tvTypeLogin.Text = typeLogin;
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            tvTypeLogin.Text = "Undefined";
+        }
+    }
+}
