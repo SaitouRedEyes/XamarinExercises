ab2967e662372e7a892840417d1b915c6f96bd52
José Augusto Mendes <zeca.saitou@gmail.com>
2021-03-24 14:43:16 -0300

    2º Bi - EX2 - App2
    2º Bi - EX2 - App2.

---
+++ 2oBi_EX2_App2/2oBi_EX2_App2/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace _2oBi_EX2_App2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [IntentFilter(new[] {"TrocaCor"}, Categories = new[] { Intent.CategoryDefault})]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            LinearLayout ll = FindViewById<LinearLayout>(Resource.Id.mainLayout);
+
+            Intent i = Intent;
+
+            if(i != null && i.Extras != null)
+            {
+                switch(i.Extras.GetString("color"))
+                {
+                    case "red": ll.SetBackgroundColor(new Android.Graphics.Color(255, 0, 0)); break;
+                    case "green": ll.SetBackgroundColor(new Android.Graphics.Color(0, 255, 0)); break;
+                    case "blue": ll.SetBackgroundColor(new Android.Graphics.Color(0, 0, 255)); break;
+                }
+            }
+        }
+	}
+}
+
