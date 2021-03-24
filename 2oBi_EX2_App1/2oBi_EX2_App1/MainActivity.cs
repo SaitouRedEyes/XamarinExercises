57a72781f05b7b25cf6535ea41eddf8878ddaac8
José Augusto Mendes <zeca.saitou@gmail.com>
2021-03-24 14:37:20 -0300

    2º Bi - EX2 - App1
    2º Bi - EX2 - App1.

---
+++ 2oBi_EX2_App1/2oBi_EX2_App1/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace _2oBi_EX2_App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Intent i;
+        Bundle parameters;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button green = FindViewById<Button>(Resource.Id.green);
+            Button blue = FindViewById<Button>(Resource.Id.blue);
+            Button red = FindViewById<Button>(Resource.Id.red);
+
+            green.Click += OnGreenButtonClicked;
+            blue.Click += OnBlueButtonClicked;
+            red.Click += OnRedButtonClicked;
+
+            i = new Intent("TrocaCor");
+            parameters = new Bundle();
+        }
+
+        private void OnRedButtonClicked(object sender, EventArgs e)
+        {
+            ChangeActivity("red");
+        }
+
+        private void OnBlueButtonClicked(object sender, EventArgs e)
+        {
+            ChangeActivity("blue");
+        }
+
+        private void OnGreenButtonClicked(object sender, EventArgs e)
+        {
+            ChangeActivity("green");
+        }
+
+        private void ChangeActivity(string color)
+        {
+            parameters.PutString("color", color);
+            i.PutExtras(parameters);
+
+            StartActivity(i);
+        }
+    }
+}
